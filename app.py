d2981f5840c274f4329e5c625c52f6cb44f5468c
Suhas G <154875420+acreatorwithvision@users.noreply.github.com>
2024-03-16 13:13:47 +0530

    uploaded files to github

---
+++ app.py
@@ -0,0 +1,74 @@
+# "from functions import get_todos, write_todos"
+#we are importing from functions.txt file
+#This will only work if both the files are in the same directory.
+import functions
+#if we have more than one function to import, can use this method
+
+while True:
+    user_action=input("type add, show, edit, complete or exit: ")
+    user_action=user_action.strip()
+
+
+    if user_action.startswith("add"):
+        todo=user_action[4:]
+
+        todos=functions.get_todos()
+
+        todos.append(todo + '\n')
+
+
+        functions.write_todos(todos)
+
+
+    elif user_action.startswith('show'):
+
+        todos=functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item=item.strip('\n')
+            row=f"{index+1}-{item}"
+            print(row)
+    elif user_action.startswith('edit'):
+        try:
+            number=int(user_action[5:])
+            print(number)
+
+            number=number-1
+
+            todos=functions.get_todos()
+
+            new_todo=input("enter a new todo: ")
+            todos[number]=new_todo+'\n'
+
+            functions.write_todos(todos)
+
+        except ValueError:
+            print("Your command is invalid")
+            continue
+
+
+    elif user_action.startswith('complete'):
+        try:
+            number=int(user_action[9:])
+
+            todos=functions.get_todos()
+
+            index=number-1
+            todo_to_remove=todos[index].strip('\n')
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            message=f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("Command is not valid")
+
+print("Byye!")
