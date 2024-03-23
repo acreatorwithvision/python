6164ede33361bde847c677ab9243209d185897ce
Suhas G <suhasg@Sinchanas-MacBook.local>
2024-03-24 01:18:21 +0530

    bonus

--- bonus5.py
+++ bonus5.py
@@ -1,3 +1,19 @@
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
+
+label1 = Sg.Text("Select Files to compress: ")
+input1 = Sg.Input()
+choose_button1 = Sg.FilesBrowse("Choose")
+
+label2 = Sg.Text("Select Destination: ")
+input2 = Sg.Input()
+choose_button2 = Sg.FolderBrowse("Choose")
+
+window=Sg.Window("File compressor", layout=[[label1, input1, choose_button1 ],
+                                            [label2, input2, choose_button2]])
+
+window.read()
+window.close()
+
+
 
  
