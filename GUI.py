4b0ce4fcde9367be1459106ecf5cccda0dbe9795
Suhas G <suhasg@Sinchanas-MacBook.local>
2024-03-25 00:41:27 +0530

    GUI

--- GUI.py
+++ GUI.py
@@ -1,15 +1,28 @@
-import functions #import functions.py file 
-import PySimpleGUI as sg#GUI module in python which needs to be downloaded before working on that.
+import functions  #import functions.py file
+import PySimpleGUI as sg  #GUI module in python which needs to be downloaded before working on that.
 
 
-label=sg.Text("Type in a to-do")
-input_box=sg.InputText(tooltip="Enter todo")
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo", key = "todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
-print("hello")
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 30))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
-
-
 
