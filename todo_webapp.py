a74400e24701a8b1d6400c19c1d588e5a782b8b6
testsite <testsite@Ubuntu-22.myguest.virtualbox.org>
2024-07-25 23:53:22 +0530

    web_app2

--- todo_webapp.py
+++ todo_webapp.py
@@ -1,10 +1,20 @@
 import streamlit as st #library used to create web app.install using pip3 install streamlit
+import functions
+def add_todo():
+    todo=st.session_state['new_todo']
+    print(todo)
+
+todos=functions.get_todos()
+
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This is to record or monitor todo app")
 
-st.checkbox("Buy Grocery")
-st.checkbox("clean")
-st.checkbox("study")
-st.checkbox("Help")
-st.checkbox("cook")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add new todo..."
+              ,on_change=add_todo,key="new_todo")
+
+print("Hello")
