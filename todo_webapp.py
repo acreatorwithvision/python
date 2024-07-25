cc2198a73e2b504599b5a16526230abfcb6e9054
acreatorwithvision <fake49403@gmail.com>
2024-07-26 02:21:41 +0530

    complete_basics

--- todo_webapp.py
+++ todo_webapp.py
@@ -1,8 +1,10 @@
 import streamlit as st #library used to create web app.install using pip3 install streamlit
 import functions
 def add_todo():
-    todo=st.session_state['new_todo']
-    print(todo)
+    todo=st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    
 
 todos=functions.get_todos()
 
@@ -11,8 +13,13 @@ st.subheader("This is my todo app")
 st.write("This is to record or monitor todo app")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox=st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo..."
               ,on_change=add_todo,key="new_todo")
