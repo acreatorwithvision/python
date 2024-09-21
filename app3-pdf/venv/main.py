5509fb1e358a7c4bca295abde94e681ce1f9197e
acreatorwithvision <fake49403@gmail.com>
2024-09-22 01:09:57 +0530

    pdf_create

--- app3-pdf/venv/main.py
+++ app3-pdf/venv/main.py
@@ -6,6 +6,7 @@ pdf = FPDF(orientation="P",unit="mm",format="A4")
 df=pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
+    for i in range()
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100,100,100)
