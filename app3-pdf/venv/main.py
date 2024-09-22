4a3b6e95baa8ff92b919d97850ef747191d08c38
acreatorwithvision <fake49403@gmail.com>
2024-09-22 13:48:03 +0530

    add_lines

--- app3-pdf/venv/main.py
+++ app3-pdf/venv/main.py
@@ -2,16 +2,40 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
-df=pd.read_csv("topics.csv")
+df = pd.read_csv("topics.csv")
+
+pdf.add_page()
 
 for index, row in df.iterrows():
-    for i in range()
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
-    pdf.line(x1=10,y1=20,x2=200,y2=20)
+
+    for y in range(20,298,10):
+        pdf.line(10,y,200,y)
+
+
+    #set header
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,100,100)
+    pdf.cell(w=0,h=0,txt=row["Topic"],align="R")
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        #set footer
+        pdf.ln(270)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 100, 100)
+        pdf.cell(w=0,h=0,txt=row["Topic"],align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+
+
 
 
 pdf.output("output.pdf")
