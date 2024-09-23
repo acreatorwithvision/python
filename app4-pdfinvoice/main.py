0f8514f03152422a3fe5ae1064297237bfee1baf
acreatorwithvision <fake49403@gmail.com>
2024-09-23 21:42:52 +0530

    add_date

--- app4-pdfinvoice/main.py
+++ app4-pdfinvoice/main.py
@@ -7,10 +7,24 @@ filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr=filename.split("-")[0]
+    invoice_nr = filename.split("-")[0]
+
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.ln(5)
+
+    filename = Path(filepath).stem
+    date = filename.split("-")[1]
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Date:{date}")
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", style="B")
+
+
     pdf.output(f"PDFs/{filename}.pdf")
