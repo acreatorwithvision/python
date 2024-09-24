fc209e2d0db2c97bcf701db73f3f883b0d183e33
acreatorwithvision <fake49403@gmail.com>
2024-09-24 06:03:59 +0530

    add_table

--- app4-pdfinvoice/main.py
+++ app4-pdfinvoice/main.py
@@ -21,10 +21,28 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     date = filename.split("-")[1]
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Date:{date}")
+    pdf.cell(w=50, h=8, txt=f"Date:{date}", ln=1)
 
+    # Add a header(here we are setting the index of the table).
+    columns = df.columns
+    print(columns)
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.set_text_color(80,80,80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
     for index, row in df.iterrows():
-        pdf.set_font(family="Times", style="B")
-
+        print(index)
+        print(row)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
