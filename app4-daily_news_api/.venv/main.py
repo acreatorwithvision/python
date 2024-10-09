91e069a69b8238eee67fb0b5aaaa6fa139dada9b
acreatorwithvision <suhas675g@gmail.com>
2024-10-10 00:12:54 +0530

    new

--- app4-daily_news_api/.venv/main.py
+++ app4-daily_news_api/.venv/main.py
@@ -3,8 +3,10 @@ from send_email import send_email
 
 # api key and url
 api_key = "04025c7256c64b828a3c119b9c96888c"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-09-08&"
-       "sortBy=publishedAt&apiKey=04025c7256c64b828a3c119b9c96888c")
+url = ("https://newsapi.org/v2/everything?q=tesla&"
+       "from=2024-09-08&"
+       "sortBy=publishedAt&"
+       "apiKey=04025c7256c64b828a3c119b9c96888c")
 
 # make a request
 request = requests.get(url)
@@ -16,7 +18,7 @@ content = request.json()
 body = ""
 for article in content["articles"]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
 
 body.encode("utf-8")
 send_email(message=body)
